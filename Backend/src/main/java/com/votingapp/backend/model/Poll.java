7f0705465a6abea37b91f7e64a8352475e6a8bf5
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-18 19:34:24 +0530

    creating a model for Poll

---
+++ Backend/src/main/java/com/votingapp/backend/model/Poll.java
@@ -0,0 +1,26 @@
+package com.votingapp.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String question;
+
+    @ElementCollection
+    private List<String> options = new ArrayList<>();
+
+    @ElementCollection
+    private List<Long> votes = new ArrayList<>();
+
+}
