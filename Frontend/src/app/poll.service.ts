5d05ccf0f2120d5515f849257c59a7585d7844a6
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-22 16:16:51 +0530

    vote api call

--- Frontend/src/app/poll.service.ts
+++ Frontend/src/app/poll.service.ts
@@ -19,5 +19,10 @@ export class PollService {
   getPolls():Observable<Poll[]>{
     return this.http.get<Poll[]>(this.baseUrl);
   }
+
+  vote(pollId: number , optionIndex: number):Observable<void>{
+    const url = `${this.baseUrl}/vote`;
+    return this.http.post<void>(url, { pollId , optionIndex });
+  }
   
 }
