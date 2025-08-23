a43f1c30712addbd7516ecd57c4884a36fcedc6d
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-23 14:58:42 +0530

    make poll ui

--- Frontend/src/app/poll/poll.component.ts
+++ Frontend/src/app/poll/poll.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PollService } from '../poll.service';
 import { Poll } from '../poll.models';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-poll',
-  imports: [],
+  imports: [ CommonModule , FormsModule],
   templateUrl: './poll.component.html',
   styleUrl: './poll.component.css'
 })
