34c66a1f2d2c812d897118481ab7653d9a947b52
Gihan_Sachith <gihansachith92@gmail.com>
2025-08-23 11:54:24 +0530

    make loadpolls function

--- Frontend/src/app/poll/poll.component.ts
+++ Frontend/src/app/poll/poll.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PollService } from '../poll.service';
+import { Poll } from '../poll.models';
 
 @Component({
   selector: 'app-poll',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './poll.component.css'
 })
 export class PollComponent {
+  polls: Poll[] = [];
+  constructor(private pollService: PollService){
+
+  }
+
+  loadPolls() {
+    this.pollService.getPolls().subscribe({
+      next: (data) => {
+          this.polls = data;
+      },
+      error: (error) => {
+        console.error("Error fetching polls: ", error);
+        
+      }
+    });
+  }
 
 }
